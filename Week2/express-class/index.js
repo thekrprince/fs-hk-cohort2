3b31a63a6f3c1a804d1c9d1ffd595b05101b968f
princekr96 <princekr700@gmail.com>
2024-04-03 00:30:04 +0530

    express new route for different fn

--- Week2/express-class/index.js
+++ Week2/express-class/index.js
@@ -1,11 +1,11 @@
-// creating an http server
-
-// express
+// Creating an http server
+// Express
 
 const express = require("express");
 
 const app = express();
 
+// Get request for Sum
 function sum(n) {
   let ans = 0;
   for (let i = 1; i <= n; i++) {
@@ -18,6 +18,48 @@ app.get("/", (req, res) => {
   const n = req.query.n; // this line will fetch the query parameter of n
   const ans = sum(n);
   res.send("Hi, your answer is " + ans);
+});
+
+const users = [
+  {
+    name: "John",
+    kidneys: [
+      {
+        healthy: true,
+      },
+    ],
+  },
+];
+
+// Get and Post request for Kidneys health
+app.get("/healthy-kidneys", (req, res) => {
+  const johnKidneys = users[0].kidneys;
+  const numbersOfKidneys = johnKidneys.length;
+  let numbersOfHealthyKidneys = 0;
+  for (let i = 0; i < numbersOfKidneys; i++) {
+    if (johnKidneys[i].healthy) {
+      numbersOfHealthyKidneys += 1;
+    }
+  }
+  const numbersOfUnhealthyKidneys = numbersOfKidneys - numbersOfHealthyKidneys;
+  res.json({
+    numbersOfKidneys,
+    numbersOfHealthyKidneys,
+    numbersOfUnhealthyKidneys,
+  });
+});
+
+// This middleware is needed to make the post request
+app.use(express.json());
+
+app.post("/healthy-kidneys", (req, res) => {
+  const isHealthy = req.body.isHealthy;
+  users[0].kidneys.push({
+    healthy: isHealthy,
+  });
+  res.json({
+    msg: "Done!",
+  });
 });
 
 app.listen(3000);
